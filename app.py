b9271fc047ec7f7a07733f18623b7bba1721c4e7
Gpt-with-ai <gptwithai123@gmail.com>
2025-11-20 16:21:00 +0530

    app.py

--- app.py
+++ app.py
@@ -1109,8 +1109,10 @@ def download_report_docx():
 # =====================================================================================
 
 if __name__ == "__main__":
+    # app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
     app.run(
         debug=True,
         host="localhost",
         port=int(os.getenv("PORT", 5050)),
     )
+
